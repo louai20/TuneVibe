0a15e49affe64a431686ad982579d5496fff6c7b
Haohao Yu <haohao@kth.se>
2024-09-24 07:20:48 +0200

    deleted "Contact" in NavBar

--- tunevibe/app/home/page.tsx
+++ tunevibe/app/home/page.tsx
@@ -63,7 +63,6 @@ export default function Home() {
             <ul className="flex space-x-4">
               <li><a href="#" className="hover:underline">Home</a></li>
               <li><a href="/aboutus" className="hover:underline">About</a></li>
-              <li><a href="#" className="hover:underline">Contact</a></li>
             </ul>
             {isLoggedIn ? (
               <DropdownMenu>
