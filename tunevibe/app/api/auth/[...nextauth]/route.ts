320f66ca826ed807a9cb2a30eda6e9584cc1355c
louai20 <alayoubi@kth.se>
2024-09-30 22:55:52 +0200

    Update route.ts

--- tunevibe/app/api/auth/[...nextauth]/route.ts
+++ tunevibe/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
