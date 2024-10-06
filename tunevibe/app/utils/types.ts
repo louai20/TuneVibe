23951772570b9b80ad83dfafce233c0e708aa707
louai20 <alayoubi@kth.se>
2024-10-07 01:10:51 +0200

    Revert "Analyze playlist, fixes small bugs"
    This reverts commit cd17260630d0ead46eb311929033b2dc20c68315, reversing
    changes made to f130f384be0eb667d5a51f8f08d1e619a9d88777.

--- tunevibe/app/utils/types.ts
+++ tunevibe/app/utils/types.ts
@@ -24,15 +24,3 @@ export interface AudioFeatures {
 export interface PlaylistAudioFeatures extends PlaylistData {
     audioFeatures: AudioFeatures[];
 }
-
-export type AudioFeatureKey =
-  | 'acousticness'
-  | 'danceability'
-  | 'energy'
-  | 'instrumentalness'
-  | 'liveness'
-  | 'speechiness'
-  | 'valence';
-
-export type FeatureKey = AudioFeatureKey | 'popularity';
-
