372f7142d843b3ff096ca846283b0af953aca043
Malki Kothalawala <malkinimesha@gmail.com>
2024-10-12 01:31:09 +0200

    Cleanup recommendations api

--- tunevibe/app/api/getRecommendations/[id]/route.ts
+++ tunevibe/app/api/getRecommendations/[id]/route.ts
@@ -1,5 +1,5 @@
+import { NextResponse } from "next/server";
 import axios from "axios";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     request: Request,
@@ -17,11 +17,7 @@ export async function GET(
             {
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
-                    Authorization:
-                        "Basic " +
-                        Buffer.from(
-                            `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
-                        ).toString("base64"),
+                    Authorization: "Basic " + Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString("base64"),
                 },
             }
         );
