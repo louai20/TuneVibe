aca4cf2b6f5ff4a4e606b77e89acc183025c9c7b
Haohao Yu <haohao@kth.se>
2024-10-11 02:04:01 +0200

    commented auth redirect

--- tunevibe/app/utils/authOptions.ts
+++ tunevibe/app/utils/authOptions.ts
@@ -80,12 +80,12 @@ export const authOptions: NextAuthOptions = {
             }
             return session;
         },
-        async redirect({ url, baseUrl }) {
-            if (url.startsWith(baseUrl)) {
-                return `${baseUrl}/playlist`;
-            }
-            return baseUrl;
-        },
+        // async redirect({ url, baseUrl }) {
+        //     if (url.startsWith(baseUrl)) {
+        //         return `${baseUrl}/playlist`;
+        //     }
+        //     return baseUrl;
+        // },
     },
     secret: process.env.NEXTAUTH_SECRET,
 };
