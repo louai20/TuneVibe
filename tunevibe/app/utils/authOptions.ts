185b5a44144d3b1acc3b33b7a002b306c44a4fed
louai20 <alayoubi@kth.se>
2024-10-15 17:56:11 +0200

    update authorize

--- tunevibe/app/utils/authOptions.ts
+++ tunevibe/app/utils/authOptions.ts
@@ -44,10 +44,8 @@ export const authOptions: NextAuthOptions = {
             });
 
             console.log("Verification email sent");
-
-            throw new Error("Email not verified. Verification link sent.");
           } catch (error) {
-            console.error("Error sending verification email:", error);
+            throw new Error("Email not verified. Verification link sent.");
           }
         }
 
@@ -64,7 +62,6 @@ export const authOptions: NextAuthOptions = {
         };
       },
     }),
-
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
