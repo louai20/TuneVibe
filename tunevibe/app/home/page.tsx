fee82012696b41cafb5edc7b8ee74cccbd337b2a
Haohao Yu <haohao@kth.se>
2024-10-15 21:59:07 +0200

    modified Analyze button position

--- tunevibe/app/home/page.tsx
+++ tunevibe/app/home/page.tsx
@@ -11,7 +11,7 @@ import useStore from "@/store/useStore";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import ImportedPlaylists from "@/components/ImportedPlaylists";
-import FloatingShapes from '@/components/FloatingShapes';
+import FloatingShapes from "@/components/FloatingShapes";
 
 export default function Home() {
     const { user, theme, setUser, toggleTheme } = useStore();
@@ -23,42 +23,19 @@ export default function Home() {
     // nextauth
     const { data: session, status } = useSession();
 
-    
-
     const handleImportClick = () => {
         signIn("spotify");
-        
     };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPlaylistUrl(e.target.value);
     };
 
-    // const router = useRouter(); // Initialize useRouter
-
-    // const extractPlaylistId = (url: string) => {
-    //     const match = url.match(/playlist\/([a-zA-Z0-9]+)/);
-    //     return match ? match[1] : null;
-    // };
-
-    // const handleAnalyze = async () => {
-    //     const playlistId = extractPlaylistId(playlistUrl);
-    //     if (playlistId) {
-    //         try {
-    //             handleFetchPlaylist(playlistUrl);
-    //             router.push(`/analysis/${playlistId}`);
-    //         } catch (error: any) {
-    //             console.error("Error navigating to analysis page:", error);
-    //             toast.error("An error occurred. Please try again.");
-    //         }
-    //     }
-    // };
-
     return (
         <div className="min-h-screen bg-background text-foreground relative overflow-x-hidden animate-fade-slide-in">
             <NavBar />
             <FloatingShapes />
-            
+
             <main className="container mx-auto px-4 py-16 z-50 relative">
                 <section className="max-w-4xl mb-4 mx-auto animate-fade-slide-in">
                     <h2 className="text-4xl font-bold mb-4">
@@ -71,55 +48,56 @@ export default function Home() {
                 </section>
 
                 <section className="mb-24 max-w-4xl mx-auto space-y-2">
-                <div className="flex gap-2 items-center">
-    <Input
-        type="text"
-        value={playlistUrl}
-        onChange={(e) => setPlaylistUrl(e.target.value)}
-        placeholder="Enter the URL of a specific playlist"
-        className="flex-grow"
-        onDragOver={(e) => e.preventDefault()}
-        onDrop={(e) => {
-            e.preventDefault();
-            const url = e.dataTransfer.getData("playlistUrl");
-            if (url) {
-                setPlaylistUrl(url);
-            }
-        }}
-    />
-    {status !== "authenticated" && <span className="text-sm">Or</span>}
-                    <div>
-                                {status === "authenticated" ? (
-                                    <Button
-                                        className="flex items-center bg-[#1DB954] hover:bg-[#1ed760] text-white"
-                                        type="button"
-                                        disabled={true}
-                                    >
-                                        <span className="icon-[mdi--spotify] mr-2 text-xl"></span>
-                                        Imported from {session.user?.name}
-                                    </Button>
-                                ) : (
-                                    <Button
-                                        className="flex items-center bg-[#1DB954] hover:bg-[#1ed760] text-white"
-                                        onClick={handleImportClick}
-                                        disabled={status === "loading"}
-                                    >
-                                        <span className="icon-[mdi--spotify] mr-2 text-xl"></span>
-                                        Import from Spotify
-                                    </Button>
-                                )}
-                            </div>
-                        </div>
+                    <div className="flex gap-2 items-center">
+                        <Input
+                            type="text"
+                            value={playlistUrl}
+                            onChange={(e) => setPlaylistUrl(e.target.value)}
+                            placeholder="Enter the URL of a specific playlist"
+                            className="flex-grow"
+                            onDragOver={(e) => e.preventDefault()}
+                            onDrop={(e) => {
+                                e.preventDefault();
+                                const url =
+                                    e.dataTransfer.getData("playlistUrl");
+                                if (url) {
+                                    setPlaylistUrl(url);
+                                }
+                            }}
+                        />
+                        {status !== "authenticated" && (
+                            <span className="text-sm">Or</span>
+                        )}
                         <div>
-                            {session?.accessToken && <ImportedPlaylists />}
+                            {status === "authenticated" ? (
+                                <Button
+                                    className="flex items-center bg-[#1DB954] hover:bg-[#1ed760] text-white"
+                                    type="button"
+                                    disabled={true}
+                                >
+                                    <span className="icon-[mdi--spotify] mr-2 text-xl"></span>
+                                    Imported from {session.user?.name}
+                                </Button>
+                            ) : (
+                                <Button
+                                    className="flex items-center bg-[#1DB954] hover:bg-[#1ed760] text-white"
+                                    onClick={handleImportClick}
+                                    disabled={status === "loading"}
+                                >
+                                    <span className="icon-[mdi--spotify] mr-2 text-xl"></span>
+                                    Import from Spotify
+                                </Button>
+                            )}
                         </div>
                         <Button
                             className="self-end"
                             onClick={() => handleFetchPlaylist(playlistUrl)} // Use the handler from the custom hook
-                            disabled={isLoading || !playlistUrl} // Disable button if no URL or 
+                            disabled={isLoading || !playlistUrl} // Disable button if no URL or
                         >
                             {isLoading ? "Loading..." : "Analyze Playlist"}
                         </Button>
+                    </div>
+                    <div>{session?.accessToken && <ImportedPlaylists />}</div>
                 </section>
                 <section className="mb-12 grid grid-cols-12 gap-4 max-w-4xl mx-auto">
                     <Card className="col-span-12 md:col-span-4">
@@ -127,43 +105,71 @@ export default function Home() {
                             <CardTitle>How It Works</CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <p className="text-balance ">Our platform takes your music experience to the next level by offering seamless integration with your playlists, advanced analysis of your favorite tracks, and personalized recommendations. Whether you're curious about your music taste or eager to discover new tunes, our service has you covered.</p>
+                            <p className="text-balance ">
+                                Our platform takes your music experience to the
+                                next level by offering seamless integration with
+                                your playlists, advanced analysis of your
+                                favorite tracks, and personalized
+                                recommendations. Whether you're curious about
+                                your music taste or eager to discover new tunes,
+                                our service has you covered.
+                            </p>
                         </CardContent>
                     </Card>
                     <div className="grid md:grid-cols-2 gap-4 col-span-12 md:col-span-8">
                         <Card>
                             <CardHeader>
-                                <CardTitle className="text-lg">1. Connect Your Music</CardTitle>
+                                <CardTitle className="text-lg">
+                                    1. Connect Your Music
+                                </CardTitle>
                             </CardHeader>
                             <CardContent>
-                                <p>Import your playlists from Spotify or enter a playlist URL.</p>
+                                <p>
+                                    Import your playlists from Spotify or enter
+                                    a playlist URL.
+                                </p>
                             </CardContent>
                         </Card>
                         <Card>
                             <CardHeader>
-                                <CardTitle className="text-lg">2. Analyze</CardTitle>
+                                <CardTitle className="text-lg">
+                                    2. Analyze
+                                </CardTitle>
                             </CardHeader>
                             <CardContent>
-                                <p>Our service analyzes the audio features of your music.</p>
+                                <p>
+                                    Our service analyzes the audio features of
+                                    your music.
+                                </p>
                             </CardContent>
                         </Card>
                         <Card>
                             <CardHeader>
-                                <CardTitle className="text-lg">3. Discover</CardTitle>
+                                <CardTitle className="text-lg">
+                                    3. Discover
+                                </CardTitle>
                             </CardHeader>
                             <CardContent>
-                                <p>Get insights about your music taste and discover new songs that match your mood.</p>
+                                <p>
+                                    Get insights about your music taste and
+                                    discover new songs that match your mood.
+                                </p>
                             </CardContent>
                         </Card>
                         <Card>
                             <CardHeader>
-                                <CardTitle className="text-lg">4. Share</CardTitle>
+                                <CardTitle className="text-lg">
+                                    4. Share
+                                </CardTitle>
                             </CardHeader>
                             <CardContent>
-                                <p>Share your music insights and discoveries with your friend!</p>
+                                <p>
+                                    Share your music insights and discoveries
+                                    with your friend!
+                                </p>
                             </CardContent>
                         </Card>
-                    {/* </div>
+                        {/* </div>
 <h3 className="text-2xl font-semibold mb-4 text-center col-span-12">
                         How It Works
                     </h3>
